e68f8665c8c410f27d2ecab89ed2b1f09ada6790
Niharika-PS <120010510+Niharika-PS@users.noreply.github.com>
2023-06-06 09:45:28 +0530

    Add files via upload

---
+++ friend.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+class A
+{
+int a,b,c;
+public:
+void input()
+{
+cout<<"Enter 3 no.s";
+cin>>a>>b>>c;
+}
+friend void large(A obj);
+}
+;
+void large(A obj)
+{
+int l=(obj.a>obj.b&&obj.a>obj.c)?obj.a:((obj.b>obj.a&&obj.b>obj.c)?obj.b:obj.c);
+cout<<"The largest no"<<l<<endl;
+}
+int main()
+{
+A obj1;
+obj1.input();
+large(obj1);
+return 0;
+}
+
